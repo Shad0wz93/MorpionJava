1265751935a117861f184127719af6df50bd5252
fabien <fabien.arnal@edu.esiee-it.fr>
2023-01-05 09:51:14 +0100

    initialisation

--- src/main/java/com/codingf/Morpion.java
+++ src/main/java/com/codingf/Morpion.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
+
 package com.codingf;
 
 public class Morpion {
+
+    static String[] board;
+
+    static String turn;
+
+    public static void main(String[] args){
+        
+        Scanner in = new Scanner(System.in);
+        board = new String[9];
+        turn = "X";
+        String winner = null;
+
+        for(int a = 0; a < 9; a++){
+            board[a] = String.valueOf( a + 1 );
+        }
+
+        System.out.println("Bienvenue");
+        printBoard();
+
+        System.out.println("Joueur 1 possede les pions X. Choisis une case :");
+    }
+
 }
+
