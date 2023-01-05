dceec0751e9974ea2568b3a4f75d5f723f2fe5dd
adamboissy2004@gmail.com <adamboissy2004@gmail.com>
2023-01-05 09:10:21 +0100

    debut fonction verif du gagnant

--- src/main/java/com/codingf/Morpion.java
+++ src/main/java/com/codingf/Morpion.java
@@ -5,6 +5,15 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Morpion {
+    static String[] board;
+    static String turn;
+
+    static String checkWinner() {
+        for(int a=0; a<8; a++){
+            String line = null;
+
+            switch(a){
+    }
 
 
 }
