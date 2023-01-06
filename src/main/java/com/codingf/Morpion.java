6245deb8b56229f8e04ad089ba350cbf6f9a59bc
guillaume <clear>
2023-01-06 16:01:39 +0100

    oui non

--- src/main/java/com/codingf/Morpion.java
+++ src/main/java/com/codingf/Morpion.java
@@ -1,23 +1,168 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import java.util.Arrays;
-
 package com.codingf;
+
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Morpion {
 
-    static void printBoard(){
+    static String[] board;
+    static String turn;
 
-        System.out.println("+---+---+---+");
-        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
-        System.out.println("|---+---+---|");
-        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
-        System.out.println("|---+---+---|");
-        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
-        System.out.println("+---+---+---+");
+
+
+        static String checkWinner(){
+
+
+            for(int a=0; a<8; a++){
+                String line = null;
+
+                switch(a){
+                    case 0:
+                        line = board[0] + board[1] + board[2];
+                        break;
+                    case 1:
+                        line = board[3] + board[4] + board[5];
+                        break;
+                    case 2:
+                        line = board[6] + board[7] + board[8];
+                        break;
+                    case 3:
+                        line = board[0] + board[3] + board[6];
+                        break;
+                    case 4:
+                        line = board[1] + board[4] + board[7];
+                        break;
+                    case 5:
+                        line = board[2] + board[5] + board[8];
+                        break;
+                    case 6:
+                        line = board[0] + board[4] + board[8];
+                        break;
+                    case 7:
+                        line = board[2] + board[4] + board[6];
+                        break;
+                }
+
+                //for X winner
+                if(line.equals("XXX")){
+                    return "X";
+                }
+
+                //for O winner
+                if(line.equals("OOO")){
+                    return "O";
+                }
+            }
+
+            for(int a=0; a < 9; a++){
+                if(Arrays.asList(board).contains(String.valueOf(a + 1))){
+                    break;
+                }
+                else if(a == 8) {
+                    return "draw";
+                }
+            }
+
+            System.out.println(
+                    turn + "'s turn. Enter a slot number to place " + turn + " in :"
+            );
+
+            return null;
+        }
+
+        static void printBoard(){
+
+            System.out.println("+---+---+---+");
+            System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+            System.out.println("|---+---+---|");
+            System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+            System.out.println("|---+---+---|");
+            System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+            System.out.println("+---+---+---+");
+        }
+
+        public static void main(String[] args){
+
+                Scanner in = new Scanner(System.in);
+
+
+                System.out.println("Bienvenue");
+
+
+
+                boolean tryagain = true ;
+                while (tryagain) {
+                    board = new String[9];
+                    turn = "X";
+                    String winner = null;
+
+                    for (int a = 0; a < 9; a++) {
+                        board[a] = String.valueOf(a + 1);
+                    }
+                    printBoard();
+                    System.out.println("Joueur 1 possede les pions X. Choisis une case :");
+                    while (winner == null) {
+
+                        int numInput = 0;
+
+                        try {
+                            numInput = in.nextInt();
+
+                            if (!(numInput > 0 && numInput <= 9)) {
+                                System.out.println("Invalide. Reessaye :");
+                            }
+                        } catch (Exception e) {
+                            System.out.println("Invalide. Reessaye :");
+                        }
+
+
+                        //décide du tour entre X et O
+                        if (board[numInput - 1].equals(String.valueOf(numInput))) {
+
+                            board[numInput - 1] = turn;
+
+                            if (turn.equals("X")) {
+                                turn = "O";
+                            } else {
+                                turn = "X";
+                            }
+
+                            printBoard();
+                            winner = checkWinner();
+                        } else {
+                            System.out.println("Case deja remplie. Selectionne une autre case :");
+                        }
+
+                    }
+
+                    //si personne ne gagne
+                    if (winner.equalsIgnoreCase("draw")) {
+                        System.out.println("Egalite ! Merci d'avoir joue.");
+                    }
+
+                    //message pour le gagnant
+                    else {
+                        System.out.println("Felicitations ! " + winner + " gagne la partie. Merci d'avoir joue !");
+                    }
+
+                    System.out.println("voulez-vous continuer ? :");
+                    Scanner scanner = new Scanner(System.in);
+                    String lettre = scanner.nextLine();
+                    if (Objects.equals(lettre, "non")) {
+                        tryagain = false;
+                    } else if (Objects.equals(lettre, "yes")) {
+                        tryagain = true;
+                        winner = null;
+                    } else {
+                        tryagain = false;
+                    }
+                }
+
     }
-
-
 }
+
+
 
 
